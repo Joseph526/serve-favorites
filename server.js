48418e79a55e5e4a61208045001a1c7c33bbe9dc
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-21 13:11:56 -0400

    Serve files working

--- server.js
+++ server.js
@@ -10,18 +10,43 @@ var server = http.createServer(handleRequest);
 
 // Create a function for handling the requests and responses coming into our server
 function handleRequest(req, res) {
+    var path = req.url;
 
-  // Here we use the fs package to read our index.html file
-  fs.readFile(__dirname + "/index.html", function(err, data) {
-
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    // an html file.
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
+    switch (path) {
+        case "/":
+            return fs.readFile(__dirname + "/index.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+            break;
+        case "/foods":
+            return fs.readFile(__dirname + "/foods.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+            break;
+        case "/movies":
+            return fs.readFile(__dirname + "/movies.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+            break;
+        case "/css":
+            return fs.readFile(__dirname + "/css.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+            break;
+        default:
+            return fs.readFile(__dirname + "/index.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+            break;
+    } 
 }
 
 // Starts our server
 server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
+    console.log("Server is listening on PORT: " + PORT);
 });
